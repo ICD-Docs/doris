a59a6a0749c91ad71b6a811cdb19c5f2e947f0ae
Marc Donada <marc.donada@outlook.com>
2025-11-06 16:47:34 +0100

    fixed yml file for ES language

--- local-live-edit.ps1
+++ local-live-edit.ps1
@@ -1,11 +1,15 @@
+${lang} = "en" # select a language: ar cs de en es fr kk pl pt ru sk sv tr zh uz 
+
+Write-Output "current language: ${lang}"
+
 # RUN THE docs for live editing. Run to create the container
 docker run -ti `
-           --name doris-mkdocs `
+           --name doris-${lang}-mkdocs `
            -p 8080:8000 `
-           -v C:/code/GitRepos/Docs/doris/en:/mkdocs `
+           -v C:/code/GitRepos/Docs/doris/${lang}:/mkdocs `
            -v C:/code/gitrepos/DocsBuilder/docbuilder/custom_readthedocs:/mkdocs/custom_readthedocs `
            --env LIVE_RELOAD_SUPPORT=true `
            polinux/mkdocs 
+
 # If it is already created, make sure it's running
-docker start doris-mkdocs
-# http://localhost:8080
+docker start doris-${lang}-mkdocs
